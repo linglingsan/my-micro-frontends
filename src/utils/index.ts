be09cc3b5aa6d417b8d381e8c59048f15eb3b3ed
linglingsan <zhangchunlin5614@163.com>
2021-10-14 06:30:44 +0800

    chore: 添加typescript类型定义

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,25 +1,27 @@
 import { useState, useEffect } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
 
-export const cleanObject = (obj) => {
+export const cleanObject = (obj: object) => {
   const result = { ...obj };
   Object.keys(obj).forEach((key) => {
+    // @ts-ignore
     const value = obj[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
-  }, []);
+  }, [callback]);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: object, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
