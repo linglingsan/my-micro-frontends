b04321340b1bf9c7e449e98b78a1cac510159c90
linglingsan <zhangchunlin5614@163.com>
2021-10-18 06:35:59 +0800

    chore: mockserver middleware

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -19,7 +19,7 @@ export const cleanObject = (obj: object) => {
 export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
-  }, [callback]);
+  }, []);
 };
 
 export const useDebounce = <T>(value: T, delay?: number): T => {
